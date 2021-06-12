46e2dc4cbdc3ffb744a5920ea4f277272eb27ff3
rickydamarsaputra <retrocode.rc@gmail.com>
2021-06-12 09:20:29 +0700

    create all page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-	purge: ["./**/*.html"],
+	purge: ["./*.html", "./pages/*.html"],
 	mode: "jit",
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px",
+			fhd: "1920px",
+		},
 		extend: {},
 	},
 	variants: {
